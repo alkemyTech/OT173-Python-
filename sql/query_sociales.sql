3c379e2253cf2d080e92fe1b001b86a6970ef20c
tutecode <tuteapps@gmail.com>
2022-03-21 16:21:23 -0300

    update

--- sql/query_sociales.sql
+++ sql/query_sociales.sql
@@ -1,17 +1,18 @@
 /* Get data from "-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES" between 01/9/2020 - 01/02/2021 */
 /* https://alkemy-labs.atlassian.net/browse/PT173-18 */
 
-SELECT 	universities AS university,
-		careers AS career,
-		inscription_dates AS inscription_date,
-		"names" AS "name",
-		sexo AS gender,
-		birth_dates AS age,
-		direccion AS postal_code,
-		locations AS "location",
-		emails AS email
+SELECT 
+	universities AS university,
+	careers AS career,
+	inscription_dates AS inscription_date,
+	"names" AS "name",
+	sexo AS gender,
+	birth_dates AS age,
+	direccion AS postal_code,
+	locations AS "location",
+	emails AS email
         
-	FROM lat_sociales_cine
+FROM lat_sociales_cine
 
 WHERE 
 	universities = "-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES"

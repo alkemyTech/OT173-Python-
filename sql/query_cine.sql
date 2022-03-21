8ab91e5a30b673e48aedb8122f5f5d2f1529c791
tutecode <tuteapps@gmail.com>
2022-03-21 12:05:05 -0300

    Get data from lat_sociales_cine

---
+++ sql/query_cine.sql
@@ -0,0 +1,17 @@
+/* Get data from "lat_sociales_cine" between 01/9/2020 - 01/02/2021 */
+
+SELECT 	universities AS university,
+		careers AS career,
+		inscription_dates AS inscription_date,
+		"names" AS "name",
+		sexo AS gender,
+		birth_dates AS age,
+		direccion AS postal_code,
+		locations AS "location",
+		emails AS email
+        
+	FROM lat_sociales_cine AS cine
+
+WHERE 
+	universities = "UNIVERSIDAD-DEL-CINE"
+	AND TO_DATE(inscription_dates, 'DD-MM-YYYY') BETWEEN '01-09-2020' AND '01-02-2021'

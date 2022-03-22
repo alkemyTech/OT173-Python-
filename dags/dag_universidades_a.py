290116aed8a4cc174499adf50084a2bc57e6728b
fedeschliamser <fede.schliamser.90@gmail.com>
2022-03-22 15:48:21 -0300

    Configure a DAG without operators for University Group A

--- dags/dag_universidades_a.py
+++ dags/dag_universidades_a.py
@@ -10,8 +10,8 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 3, 19)
 ) as dag:
-    tarea_1=DummyOperator(task_id="Query_Flores")         #I will use the PostgresOperator to do the query from "Universidad de Flores"
-    tarea_2=DummyOperator(task_id="Query_Villa_Maria")    #I will use the PostgresOperator to do the query from "Universidad de Villa Maria" 
+    tarea_1=DummyOperator(task_id="Query_Flores")         #I will use the PythonOperator to do the query from "Universidad de Flores"
+    tarea_2=DummyOperator(task_id="Query_Villa_Maria")    #I will use the PythonOperator to do the query from "Universidad de Villa Maria" 
     tarea_3=DummyOperator(task_id="Process_Data")         #I will use the PythonOperator to process the data with Pandas
     tarea_4=DummyOperator(task_id="Charge_Data")          #I will use the S3Operator to charge the data
 

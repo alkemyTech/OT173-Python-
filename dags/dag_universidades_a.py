39c713221949c8f0f35917c603f84b3986b096f1
fedeschliamser <fede.schliamser.90@gmail.com>
2022-03-22 16:50:31 -0300

    Configure a DAG without operators for University Group A

--- dags/dag_universidades_a.py
+++ dags/dag_universidades_a.py
@@ -1,5 +1,7 @@
 from datetime import timedelta, datetime
+
 from airflow import DAG
+
 from airflow.operators.dummy import DummyOperator
 
 with DAG(
@@ -9,8 +11,8 @@ with DAG(
     start_date=datetime(2022, 3, 19)
 ) as dag:
     tarea_1 = DummyOperator(task_id="Query_Flores")      # PythonOperator to do the query
-    tarea_2 = DummyOperator(task_id="Query_Villa_Maria") # PythonOperator to do the query
+    tarea_2 = DummyOperator(task_id="Query_Villa_Maria")  # PythonOperator to do the query
     tarea_3 = DummyOperator(task_id="Process_Data")      # PythonOperator to process the data with Pandas
-    tarea_4 = DummyOperator(task_id="Charge_Data") # Charge the data with S3Operator
+    tarea_4 = DummyOperator(task_id="Charge_Data")  # Charge the data with S3Operator
 
-    [tarea_1,tarea_2] >> tarea_3 >> tarea_4
+    [tarea_1, tarea_2] >> tarea_3 >> tarea_4

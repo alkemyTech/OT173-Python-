c9fd4cfd43063d2b08a948403da25a693bf79ecc
ochoajuanm <juan.martin.ochoa.92@gmail.com>
2022-03-23 17:19:27 -0300

    Fix errors

--- dags/dag_universidades_h.py
+++ dags/dag_universidades_h.py
@@ -4,9 +4,9 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 
 with DAG(
-    'dag_universidades_h', 
-    description='DAG for processing data from Universidad Del Cine and Universidad De Buenos Aires', 
-    schedule_interval=timedelta(hours=1), 
+    'dag_universidades_h',
+    description='DAG for processing data from Universidad Del Cine and Universidad De Buenos Aires',
+    schedule_interval=timedelta(hours=1),
     start_date=datetime(2022, 3, 20)
 ) as dag:
     # Extract data with SQL query - Postgres operator
@@ -16,7 +16,7 @@ with DAG(
     # Transform data with Pandas - Python operator
     transform_uba = DummyOperator(task_id='transform_uba')
     transform_cine = DummyOperator(task_id='transform_cine')
-    
+
     # Load data to S3 - S3 operator
     load_uba = DummyOperator(task_id='load_uba')
     load_cine = DummyOperator(task_id='load_cine')

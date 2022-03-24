fdd2d3e37d794e72d3ee9d46b82c028413b774ac
fedeschliamser <fede.schliamser.90@gmail.com>
2022-03-24 00:13:39 -0300

    Configure a Retries for DAG ofniversity Group A

---
+++ dags/dag_universidades_a.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+
+default_args = {
+    'retries': 5,  #Try in 5 opportunities to run the script
+    'retry_delay': timedelta(minutes=5)  #Wait 5 minutes to try to run the script again
+}

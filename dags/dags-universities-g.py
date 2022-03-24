38c0f248f0f0d56fe26227caa1e355c4164a5159
tutecode <tuteapps@gmail.com>
2022-03-24 09:54:59 -0300

    update dags.py

--- dags/dags-universities-g.py
+++ dags/dags-universities-g.py
@@ -12,7 +12,7 @@ default_args = {
     # 'email_on_failure': False,
     # 'email_on_retry': False,
     # 'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    # 'retry_delay': timedelta(minutes=5),
 }
 
 
@@ -26,21 +26,21 @@ def data_process():
     logging.info('Processing data')
 
 
-# Save date in S3
+# Save data in S3
 def save_data():
     logging.info('Saving data in S3')
 
 
 with DAG(
     'dag-g-PT173-34',
-    default_args = default_args,
-    description = 'Dags Universities',
-    schedule_interval = '@hourly',
+    default_args=default_args,
+    description='Dags Universities',
+    schedule_interval='@hourly',
     # start_date = datetime(2022, 1, 19),
-    tags = ['dags'],
+    tags=['universities'],
 ) as dag:
     getdata_task = PythonOperator(task_id='get_data', python_callable=get_data)
     dataprocess_task = PythonOperator(task_id='data_process', python_callable=data_process)
     savedata_task = PythonOperator(task_id='save_data', python_callable=save_data)
-    
+
     getdata_task >> dataprocess_task >> savedata_task

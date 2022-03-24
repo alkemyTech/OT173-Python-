c39b150ab303d893628687e4971b83c17a2cf39f
franco rossi <kpitan022@gmail.com>
2022-03-24 01:34:49 -0300

    fix imports

--- dags/dag_universidades_c.py
+++ dags/dag_universidades_c.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
-
+# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+# Operators; we need this to operate!
 from airflow.operators.dummy import DummyOperator
 
 

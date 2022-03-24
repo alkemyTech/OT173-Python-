1a1c146f7308ea1ae447135136c45eb5a2a83a0f
ochoajuanm <juan.martin.ochoa.92@gmail.com>
2022-03-24 11:26:25 -0300

    Configure logger for Universities of group H

--- dags/dag_universidades_h.py
+++ dags/dag_universidades_h.py
@@ -1,6 +1,9 @@
-from datetime import timedelta
+import logging
 
-default_args = {
-    'retries': 5,
-    'retry_delay': timedelta(minutes=5)
-}
+logging.basicConfig(
+    format='%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d',
+    level=logging.INFO,
+    filemode="a"
+    )
+logger = logging.getLogger(__name__)

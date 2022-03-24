59e8e0787b6b37642d5512dab3144cb319b69afd
fedeschliamser <fede.schliamser.90@gmail.com>
2022-03-24 15:59:07 -0300

    Configure logs for DAG ofniversity Group A

---
+++ dags/dag_universidades_a.py
@@ -0,0 +1,8 @@
+import logging
+
+from time import strftime
+
+logging.basicConfig(level=logging.INFO, datefmt=strftime("%Y-%m-%d"),
+                    format='%(asctime)s - %(name)s - %(message)s')
+
+logger = logging.getLogger("Universidades_A")

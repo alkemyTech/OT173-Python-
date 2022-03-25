0ee1c4b378051427ce9e0033e39c8d0ed533e24e
Alonso hernandez <ing.alonsoehg@hotmail.com>
2022-03-25 12:22:44 -0600

    PR fix: comments

--- dags/dag_universidades_e.py
+++ dags/dag_universidades_e.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-# add attribute default_args into DAG: default_args = default_args.
+# added attribute default_args into DAG: default_args = default_args.
 default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=5)

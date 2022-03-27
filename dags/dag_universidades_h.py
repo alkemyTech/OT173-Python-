05e2266ede8881828430e3874cd558498c372afa
Juan Ochoa <juan.martin.ochoa.92@gmail.com>
2022-03-27 18:00:30 -0300

    Config logs h pt173 51 (#24)
    * update dags.py
    
    * delete dags directory
    
    * Configure DAG retries for Universities of group H
    
    * Configure logger for Universities of group H
    
    * Change log format
    
    Co-authored-by: tutecode <tuteapps@gmail.com>
    Co-authored-by: TuteCode <54072234+tutecode@users.noreply.github.com>
    Co-authored-by: ochoajuanm <“juan.martin.ochoa.92@gmail.com”>
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dags/dag_universidades_h.py
+++ dags/dag_universidades_h.py
@@ -1,7 +1,16 @@
+import logging
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(message)s',
+    datefmt='%Y-%m-%d',
+    level=logging.INFO,
+    filemode="a"
+    )
+logger = logging.getLogger(__name__)
 
 default_args = {
     'retries': 5,

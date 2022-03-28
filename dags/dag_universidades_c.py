1b48c2b31d2c5f93c204bbf04f1046ee77ec7740
franco rossi <kpitan022@gmail.com>
2022-03-28 18:16:48 -0300

    added function extraxt and PythonOperators

--- dags/dag_universidades_c.py
+++ dags/dag_universidades_c.py
@@ -1,8 +1,12 @@
 import logging
 from datetime import datetime, timedelta
+from os import path, makedirs
 
+import pandas as pd
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.hooks.postgres_hook import PostgresHook
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(message)s',
@@ -29,18 +33,53 @@ with DAG(
         default_args=default_args
         ) as dag:
 
-    # PythonOperator and PostgresHook to get the data from the database and
-    #  save it in a csv file in the include/tmp folder of the project.
-    extract_task_palermo = DummyOperator(
-        task_id='extract_palermo',
-        dag=dag,
-    )
+    def extract(query, university):
+        """PythonOperator and PostgresHook to get the data from the
+        database and save it in a csv file in the include/tmp folder
+        of the project.
+
+        Args:
+            query (str): The query file to be executed in the database.
+            example: query_universidades_c.sql
+            university (str): The name of the university to be extracted.
+            example: 'universidad_de_los_andes'
+        """
+        base_path = path.abspath(path.join(path.dirname(__file__), ".."))
+        path_tmp = path.abspath(path.join(base_path, 'include', 'tmp'))
+        if not path.isdir(path_tmp):
+            makedirs(path_tmp)
+        path_csv = path.join(path_tmp, university)
+        path_query = path.join(base_path, 'sql', query)
+        pg_hook = PostgresHook(
+            postgres_conn_id='alkemy_db',
+            schema='training'
+        )
+        with open(path_query, 'r') as file:
+            pg_hook = PostgresHook(
+                postgres_conn_id='alkemy_db',
+                schema='training'
+            )
+            pg_conn = pg_hook.get_conn()
+            df_to_csv = pd.read_sql(file.read(), pg_conn)
+            logger.info(f'{university} - {df_to_csv.shape}')
+            df_to_csv.to_csv(path_csv)
 
     # PythonOperator and PostgresHook to get the data from the database and
     #  save it in a csv file in the include/tmp folder of the project.
-    extract_task_jujuy = DummyOperator(
+    extract_task_palermo = PythonOperator(
+        task_id='extract_palermo',
+        python_callable=extract,
+        op_kwargs={
+            'query': 'query_palermo.sql',
+            'university': 'palermo.csv'
+        }
+    )
+    extract_task_jujuy = PythonOperator(
         task_id='extract_jujuy',
-        dag=dag,
+        python_callable=extract,
+        op_kwargs={
+            'query': 'query_universidad_nacional_de_jujuy.sql',
+            'university': 'universidad_nacional_de_jujuy.csv'}
     )
     # PythonOperator and pandas to read the csv file and create a /dataframe.
     # the procesed data are saved in .txt file
@@ -57,4 +96,3 @@ with DAG(
     )
 
     [extract_task_palermo, extract_task_jujuy] >> transform_task >> load_task
-    

431d3f95d72aeb696584d3e5fa6e0af45ffa509a
guillermouzner <guillermouz16@gmail.com>
2022-04-02 11:48:43 -0300

    logging config for dag f

--- dags/dag_universities_f.py
+++ dags/dag_universities_f.py
@@ -8,9 +8,9 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(message)s',
-     datefmt='%Y-%m-%d',
-     level=logging.INFO,
-     filename='test.log')
+    datefmt='%Y-%m-%d',
+    level=logging.INFO,
+    filename='test.log')
 
 with DAG(
     'dag_universities_f',
